c5499ef78e318ceaf6d1e098b0ec19aa150e43f2
zk <17600963531@126.com>
2026-05-09 17:24:45 +0800

    Self-host site fonts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import 'highlight.js/styles/github-dark.css'
+import '@fontsource/inter/latin-400.css'
+import '@fontsource/inter/latin-500.css'
+import '@fontsource/inter/latin-600.css'
+import '@fontsource/inter/latin-700.css'
+import '@fontsource/space-grotesk/latin-500.css'
+import '@fontsource/space-grotesk/latin-700.css'
 import siteConfig from './config/site'
 
 document.title = `${siteConfig.siteName} - ${siteConfig.tagline}`
