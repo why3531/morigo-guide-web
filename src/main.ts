3c4ccede2aadc7803188e029edb2a87ec9c95c35
zk <17600963531@126.com>
2026-05-13 15:47:01 +0800

    Update site

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import 'highlight.js/styles/github-dark.css'
+import { inject } from '@vercel/analytics'
 import '@fontsource/inter/latin-400.css'
 import '@fontsource/inter/latin-500.css'
 import '@fontsource/inter/latin-600.css'
@@ -11,5 +12,7 @@ import '@fontsource/space-grotesk/latin-700.css'
 import siteConfig from './config/site'
 
 document.title = `${siteConfig.siteName} - ${siteConfig.tagline}`
+
+inject()
 
 createApp(App).mount('#app')
